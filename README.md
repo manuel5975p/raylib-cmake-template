0e3979c310ecc9fd210d4180c20eabf6efcc57f5
Manuel <manuel.winkler@gmx.ch>
2024-06-19 01:24:23 +0200

    Add web support

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
+# Extent
+Demonstrates a the build process of a **simple & portable** raylib application for Desktop / Web.
+
+- (Static) assets passed via `--preload-file` for Web
+- Dynamic files using IDBFS (browser storage)
+
 # Usage
 ```sh
 git clone https://github.com/manuel5975p/raylib-cmake-template.git
 cd raylib-cmake-template
-mkdir build
-cd build
+mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug #Can also be =Release, =RelWithDebInfo, nothing defaults to Debug
 make -j4
 ./main
 ```
-# Usage with VSCode
+## Usage for Web
+Requires [emscripten](https://emscripten.org/docs/getting_started/downloads.html).
+
+```sh
+mkdir build_web && cd build_web
+emcmake cmake .. -DCMAKE_BUILD_TYPE=Release -DPLATFORM=Web
+make -j4
+```
+## Usage with VSCode
 
 - Open the `raylib-cmake-template` directory in VSCode
 - Make sure the "C/C++" extension as well "CMake Tools" are installed
 - Also have a compiler installed (e.g. [the latest w64devkit](https://github.com/skeeto/w64devkit/releases))
 - Hit the debug, build button or hit Ctrl+Shift+P for the command menu and run "CMake: Debug" or something similar
+
--- main.c
+++ main.c
@@ -1,18 +1,46 @@
 #include <raylib.h>
 #include <stdio.h>
 #include <rcamera.h>
+#include <time.h>
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
 #define LOG(X) printf("%s: %d", __FILE__, __LINE__, X)
 RenderTexture rt;
-void hauptloop(){
+
+#ifdef __EMSCRIPTEN__
+EM_JS(void, idbfs_put, (const char* filename, const char* str), {
+    FS.writeFile(UTF8ToString(filename), UTF8ToString(str));
+    FS.syncfs(false,function (err) {
+        assert(!err);
+    });
+});
+EM_JS(char*, idbfs_get, (const char* filename), {
+    var arr = FS.readFile(UTF8ToString(filename));
+    var jsString = new TextDecoder().decode(arr);
+    var lengthBytes = lengthBytesUTF8(jsString)+1;
+    //console.log(jsString);
+    var stringOnWasmHeap = _malloc(lengthBytes);
+    stringToUTF8(jsString, stringOnWasmHeap, lengthBytes);
+    return stringOnWasmHeap;
+});
+#endif
+const char* someResource = 0;
+
+
+void gameLoop(){
     BeginDrawing();
     BeginTextureMode(rt);
     ClearBackground(CLITERAL(Color){0, 0, 0, 255});
     DrawCircle(GetMouseX(), GetMouseY(), 50, CLITERAL(Color){255,255,0,255});
     EndTextureMode();
     DrawTexture(rt.texture, 0, 0, CLITERAL(Color){255,255,255,255});
+    DrawFPS(0, 0);
+    #ifdef __EMSCRIPTEN__
+    char* text = idbfs_get("file.txt");
+    DrawText(TextFormat("Dynamic file content: %s", text), 0, 30, 20, WHITE);
+    #endif
+    DrawText(TextFormat("Static file content: %s", someResource), 0, 60, 20, WHITE);
     EndDrawing();
 }
 int main(){
@@ -21,15 +49,25 @@ int main(){
     
     InitWindow(screenWidth, screenHeight, "rtextures");
     SetTargetFPS(144);
-    
-    //LOG("OpenGL version: " << glGetString(GL_VERSION));
     rt = LoadRenderTexture(screenWidth, screenHeight);
+    someResource = LoadFileText("../resources/dummy.txt");
+
     #ifdef __EMSCRIPTEN__
-    emscripten_set_main_loop(hauptloop, 0, 0);
-    //emscripten_sleep(10000);
+    EM_ASM(
+        FS.mkdir('/work');
+        FS.mount(IDBFS, {}, '/work');
+        FS.syncfs(true, function (err){
+            assert(!err);
+        });
+    );
+    struct timespec ts = {.tv_sec = 0,  .tv_nsec = 100000000};
+    struct timespec ts2 = {.tv_sec = 0, .tv_nsec = 100000000};
+    nanosleep(&ts, &ts2);
+    idbfs_put("file.txt", "Some dynamic file contents...\n");
+    emscripten_set_main_loop(gameLoop, 0, 0);
     #else
     while(!WindowShouldClose()){
-        hauptloop();
+        gameLoop();
     }
     #endif
     return 0;
